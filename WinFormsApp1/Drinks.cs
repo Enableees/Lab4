e821cc8ca3ce50f72348b8c1af6589166c45c555
Enableees <vladshumkov@bk.ru>
2026-04-06 23:26:27 +0800

    enum для juice, вывод enum на экран на Русском

--- WinFormsApp1/Drinks.cs
+++ WinFormsApp1/Drinks.cs
@@ -18,26 +18,44 @@ namespace WinFormsApp1
             }
         }
 
+        public enum JuiceType { apple, orange, grape, pomegranate, mango };
         public class Juice : Beverage
         {
-            public string FruitUsed = "";
+            public JuiceType FruitUsed = JuiceType.apple;
             public bool HasPulp = false;
 
+            private string GetFruitName()
+            {
+                switch (FruitUsed)
+                {
+                    case JuiceType.apple:
+                        return "яблоко";
+                    case JuiceType.orange:
+                        return "апельсин";
+                    case JuiceType.grape:
+                        return "виноград";
+                    case JuiceType.pomegranate:
+                        return "гранат";
+                    case JuiceType.mango:
+                        return "манго";
+                    default:
+                        return FruitUsed.ToString();
+                }
+            }
             public override String GetInfo()
             {
                 var str = "Я сок";
                 str += base.GetInfo();
-                str += String.Format("\nИспользуемый фрукт: {0}", this.FruitUsed);
+                str += String.Format("\nИспользуемый фрукт: {0}", GetFruitName());
                 str += String.Format("\nНаличие мякоти: {0}", this.HasPulp ? "есть" : "нет");
                 return str;
             }
             public static Juice Generate()
             {
-                string[] fruits = { "апельсин", "яблоко", "виноград", "гранат", "манго" };
                 return new Juice
                 {
                     Volume = 200 + rnd.Next() % 300,
-                    FruitUsed = fruits[rnd.Next(fruits.Length)],
+                    FruitUsed = (JuiceType)rnd.Next(5),
                     HasPulp = rnd.Next() % 2 == 0
                 };
             }
@@ -49,11 +67,27 @@ namespace WinFormsApp1
             public SodaType Type = SodaType.cola;
             public int BubblesCount = 0;
 
+            private string GetSodaType()
+            {
+                switch (Type)
+                {
+                    case SodaType.cola:
+                        return "кола";
+                    case SodaType.lemonade:
+                        return "лимонад";
+                    case SodaType.orange:
+                        return "апельсиновая";
+                    case SodaType.tonic:
+                        return "тоник";
+                    default:
+                        return Type.ToString();
+                }
+            }
             public override String GetInfo()
             {
                 var str = "Я газировка";
                 str += base.GetInfo();
-                str += String.Format("\nВид: {0}", this.Type);
+                str += String.Format("\nВид: {0}", GetSodaType());
                 str += String.Format("\nКоличество пузыриков: {0}", this.BubblesCount);
                 return str;
             }
@@ -74,12 +108,30 @@ namespace WinFormsApp1
             public double Strength = 0;
             public AlcoholType Type = AlcoholType.beer;
 
+            private string GetAlcoholType()
+            {
+                switch (Type)
+                {
+                    case AlcoholType.beer:
+                        return "пиво";
+                    case AlcoholType.wine:
+                        return "вино";
+                    case AlcoholType.vodka:
+                        return "водка";
+                    case AlcoholType.whiskey:
+                        return "виски";
+                    case AlcoholType.cocktail:
+                        return "коктейль";
+                    default:
+                        return Type.ToString();
+                }
+            }
             public override String GetInfo()
             {
                 var str = "Я алкогольный напиток";
                 str += base.GetInfo();
-                str += String.Format("\nКрепость: {0}%", this.Strength);
-                str += String.Format("\nТип: {0}", this.Type);
+                str += String.Format("\nКрепость: {0:F1}%", this.Strength);
+                str += String.Format("\nТип: {0}", GetAlcoholType());
                 return str;
             }
             public static Alcohol Generate()
